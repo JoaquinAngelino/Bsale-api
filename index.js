4d975251bc603dde6a9d803c9ba2285a27f9d566
JoaquinAngelino <joacoangelino@gmail.com>
2022-11-09 17:45:08 -0300

    s

--- index.js
+++ index.js
@@ -15,9 +15,12 @@ server.use((_req, res, next) => {
 });
 
 server.use('/', router)
+server.use((req, res, next) => {
+  res.status(404).send({message: "endpoint not found"})
+})
 
 const port = 3001
 
-server.listen(port,()=>{
+server.listen(port, () => {
   console.log('server running on port 3001');
 })
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -5,16 +5,15 @@ const productRoutes = Router()
 
 productRoutes.get('/', async (req, res) => {
   const page = req.query.page - 1 || 0
-  let whereName = req.query.name ? ` WHERE name LIKE "%${req.query.name}%"` : ''
-
+  let whereName = req.query.name ? ` WHERE product.name LIKE "%${req.query.name}%"` : ''
   try {
     const count = (await pool.query(`SELECT COUNT(*) FROM product${whereName}`))[0][0]
-    const result = (await pool.query(`SELECT * FROM product${whereName} LIMIT ${page * 6}, 6`))[0]
+    const result = (await pool.query(`SELECT * FROM product ${whereName} LIMIT ${page * 6}, 6 ;`))[0]
     console.log("products routes");
     res.send({ count: count["COUNT(*)"], pages: Math.ceil(count["COUNT(*)"] / 6), result })
   } catch (error) {
     console.log(error);
-    res.send({count: 0, pages: 0, result: [], error})
+    res.send({count: 0, pages: 0, result: [], error })
   }
 })
 
