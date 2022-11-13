f00027f9375fd83518a4081650c28b6524e72e91
JoaquinAngelino <joacoangelino@gmail.com>
2022-11-13 04:44:12 -0300

    Update product.route.js

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -6,17 +6,17 @@ const productRoutes = Router()
 productRoutes.get('/', async (req, res) => {
   const { page, category, name } = req.query
   let whereName = name ? ` WHERE product.name LIKE "%${name}%"` : ``
-  if (category && name) {
-    whereName += ` AND `
-  } else {
-    if (category) {
-      whereName += ` WHERE `
+  let whereCategory = ``
+  if (category) {
+    if (name) {
+      whereCategory = ` AND product.category = ${category} `
+    } else {
+      whereCategory = ` WHERE product.category = ${category} `
     }
   }
-  let whereCategory = category ? `product.category = ${category} ` : ``
   try {
     const count = (await pool.query(`SELECT COUNT(*) FROM product${whereName}${whereCategory}`))[0][0]
-    const result = (await pool.query(`SELECT * FROM product ${whereName} ${whereCategory} LIMIT ${(page - 1 || 0) * 6}, 6 ;`))[0]
+    const result = (await pool.query(`SELECT * FROM product ${whereName}${whereCategory} LIMIT ${(page - 1 || 0) * 6}, 6 ;`))[0]
     res.send({ count: count["COUNT(*)"], pages: Math.ceil(count["COUNT(*)"] / 6), result })
   } catch (error) {
     console.log(error);
