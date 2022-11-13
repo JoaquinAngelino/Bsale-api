f09e225d674e82467694a8aa5b24f735a320534f
JoaquinAngelino <joacoangelino@gmail.com>
2022-11-13 15:35:10 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,6 @@ import productRoutes from "./product.route.js";
 
 const router = Router()
 
-router.use('/', (req, res )=>{
-  res.send("hola")
-})
 router.use('/product', productRoutes)
 router.use('/category', categoryRoutes)
 
